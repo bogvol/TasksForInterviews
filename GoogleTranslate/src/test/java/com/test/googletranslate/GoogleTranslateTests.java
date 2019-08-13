99537cb2c89842a42fe2192693ecf4f5b294aaa6
Bohdan Volovyk <volovukbogdan@gmail.com>
2019-08-13 22:52:38 +0200

    Rearranged

--- GoogleTranslate/src/test/java/com/test/googletranslate/GoogleTranslateTests.java
+++ GoogleTranslate/src/test/java/com/test/googletranslate/GoogleTranslateTests.java
@@ -13,56 +13,56 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class GoogleTranslateTests {
-    private WebDriver driver;
-    private WebDriver driverForConcurrentSession = null;
+	private WebDriver driver;
+	private WebDriver driverForConcurrentSession = null;
 
-    @Before
-    public void setUp() {
-        driver = new ChromeDriver();
-        navigateToSite(driver, "https://www.google.com");
-    }
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
+		navigateToSite(driver, "https://www.google.com");
+	}
 
-    @After
-    public void tearDown() {
-        driver.quit();
-        if (driverForConcurrentSession != null) driverForConcurrentSession.quit();
-    }
+	@After
+	public void tearDown() {
+		driver.quit();
+		if (driverForConcurrentSession != null) driverForConcurrentSession.quit();
+	}
 
-    @Test
-    public void searchResult() {
-        searchRequestedPhrase(driver, "google translate");
-        String linkEN = retrieveLinkToGoogleTranslate(driver);
-        driverForConcurrentSession = new ChromeDriver();
-        navigateToSite(driverForConcurrentSession, linkEN);
-        String translation =
-                getTextTranslation(driverForConcurrentSession, "English", "Polish", "luxoft test task");
+	@Test
+	public void searchResult() {
+		searchRequestedPhrase(driver, "google translate");
+		String linkEN = retrieveLinkToGoogleTranslate(driver);
+		driverForConcurrentSession = new ChromeDriver();
+		navigateToSite(driverForConcurrentSession, linkEN);
+		String translation =
+				getTextTranslation(driverForConcurrentSession, "English", "Polish", "luxoft test task");
 
-        assertThat(translation, containsString("zadaniem testu"));
-    }
+		assertThat(translation, containsString("zadaniem testu"));
+	}
 
-    private String getTextTranslation(WebDriver driver,
-                                      String from,
-                                      String to,
-                                      String text) {
-        GoogleTranslatePage translator = new GoogleTranslatePage(driver);
-        translator.chooseSourceAndTargetLanguage(from, to);
-        return translator.getTranslation(text);
-    }
+	private String getTextTranslation(WebDriver driver,
+	                                  String from,
+	                                  String to,
+	                                  String text) {
+		GoogleTranslatePage translator = new GoogleTranslatePage(driver);
+		translator.chooseSourceAndTargetLanguage(from, to);
+		return translator.getTranslation(text);
+	}
 
-    private void navigateToSite(WebDriver driver, String link) {
-        driver.manage().window().maximize();
-        driver.get(link);
-    }
+	private void navigateToSite(WebDriver driver, String link) {
+		driver.manage().window().maximize();
+		driver.get(link);
+	}
 
-    private void searchRequestedPhrase(WebDriver driver, String requestedPhrase) {
-        GoogleMainPage mainPage = new GoogleMainPage(driver);
-        mainPage.search(requestedPhrase);
-    }
+	private void searchRequestedPhrase(WebDriver driver, String requestedPhrase) {
+		GoogleMainPage mainPage = new GoogleMainPage(driver);
+		mainPage.search(requestedPhrase);
+	}
 
-    private String retrieveLinkToGoogleTranslate(WebDriver driver) {
-        ResultsPage searchResult = new ResultsPage(driver);
-        // Get link to Google Translate EN (not depends on localization)
-        String link = searchResult.retrieveItemLinkFromResultsList();
-        return link.replaceAll("(hl=)[a-z]+", "hl=en");
-    }
+	private String retrieveLinkToGoogleTranslate(WebDriver driver) {
+		ResultsPage searchResult = new ResultsPage(driver);
+		// Get link to Google Translate EN (not depends on localization)
+		String link = searchResult.retrieveItemLinkFromResultsList();
+		return link.replaceAll("(hl=)[a-z]+", "hl=en");
+	}
 }
